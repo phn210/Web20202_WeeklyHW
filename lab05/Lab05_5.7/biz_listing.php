65e7294670d45dbb892f0a37570d141ecf49d2a8
Toanla087 <minhtoan0807@gmail.com>
2021-04-30 01:32:38 +0700

    add Lab05_5.7

---
+++ lab05/Lab05_5.7/biz_listing.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Business Listings</title>
+    <style>
+        table, td, th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: left;
+            width: 200px;
+        }
+        .title {
+            display: block;
+        }
+        .title:link {
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Business Listings</h1>
+    <form action="" method="POST">
+        <table>
+            <tr>
+                <th style="width:200px;">Clicks on one, or control-click on mutiple categories</th>
+            </tr>
+            <tr>
+                <td><a href="" class = "title">Automative Parts and Supplies</a></td>
+            </tr>
+            <tr>
+                <td><a href="" class = "title">Seafood Stores and Restaurants</a></td>
+            </tr>
+            <tr>
+                <td><a href="" class = "title">Health and Beauty</a></td>
+            </tr>
+            <tr>
+                <td><a href="" class = "title">School and Colleges</a></td>
+            </tr>
+            <tr>
+                <td><a href="" class = "title">Comunity Sports and Recreations</a></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
